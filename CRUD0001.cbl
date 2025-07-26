c89a543044761814c2cf095129631b4f93873c6d
brunokian <brunokian@hotmail.com>
2025-07-25 22:43:59 -0300

    criando arquivo para armazenar dados

--- CRUD0001.cbl
+++ CRUD0001.cbl
@@ -9,15 +9,29 @@
       ***************************************************************
        ENVIRONMENT DIVISION.
       *************************************************************
-      * INPUT-OUTPUT SECTION.
-      * FILE-CONTROL.
-      *     SELECT CLIENTES ASSIGN TO 'D:'.
+       INPUT-OUTPUT SECTION.
+
+       FILE-CONTROL.
+           SELECT CLIENTES ASSIGN TO 'CLIENTES.DAT'
+           ORGANIZATION IS INDEXED
+           ACCESS MODE IS RANDOM
+           FILE STATUS IS CLIENTES-STATUS
+           RECORD KEY IS CLIENTES-CHAVE.
        DATA DIVISION.
        FILE SECTION.
+       FD CLIENTES.
+       01 CLIENTES-REG.
+           05 CLIENTES-CHAVE.
+               10 CLIENTES-FONE PIC 9(09).
+           05 CLIENTES-NOME PIC X(30).
+           05 CLIENTES-EMAIL PIC X(40).
+
+
        WORKING-STORAGE SECTION.
        77 WRK-OPTION PIC X(1).
        77 WRK-MODULO PIC X(25) VALUE '__TITLE__'.
        77 WRK-TECLA PIC X(1).
+       77 CLIENTES-STATUS PIC 9(2).
        SCREEN SECTION.
        01 TELA.
            05 LIMPA-TELA.
@@ -29,7 +43,7 @@
                10 LINE 02 COLUMN 01 PIC X(25) ERASE EOL
                BACKGROUND-COLOR 1 FROM WRK-MODULO.
 
-      ***************************************************************
+      ******************************************************************
       *No cobol, não há "redesenho automático da tela" como nas
       *interfaces gráficas modernas.
       *
@@ -37,7 +51,7 @@
       *apenas sobrepõe os caracteres, e não apaga o que sobrou se o
       *novo texto for mais curto. Por isso, é interessante criar uma
       * função para limpar a tela
-      ************************************************************
+      ******************************************************************
 
        01 MENU.
            03 LINE 07 COLUMN 15 VALUE '1 - INCLUIR'.
@@ -51,6 +65,7 @@
 
       *****************************************************************
        PROCEDURE DIVISION.
+      ******************************************************************
        0001-PRINCIPAL.
            PERFORM 1000-INICIAR.
            PERFORM 2000-PROCESSAR.
@@ -59,6 +74,14 @@
 
 
        1000-INICIAR.
+           OPEN I-O CLIENTES
+               IF CLIENTES-STATUS = 35 THEN
+                   OPEN OUTPUT CLIENTES
+                   CLOSE CLIENTES
+                   OPEN I-O CLIENTES
+               END-IF
+
+
            DISPLAY TELA.
            ACCEPT MENU.
 
@@ -71,12 +94,14 @@
                    CONTINUE
                WHEN OTHER
                    IF WRK-OPTION NOT EQUAL 'X'
-                       DISPLAY 'ENTRE COM OPCAO VALIDA'
+                       DISPLAY 'ENTRE COM OPCAO VALIDA' AT 1620
+
+
                    END-IF
            END-EVALUATE.
 
        3000-FINALIZAR.
-           CONTINUE.
+           CLOSE CLIENTES.
 
        5000-INCLUIR.
            MOVE 'MODULO INCLUSAO' TO WRK-MODULO.
